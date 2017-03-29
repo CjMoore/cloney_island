354377375ccc55a53f34183a8295142aedac3d38
Charlotte Moore <mooc123@gmail.com>
2017-03-29 16:12:14 -0600

    feature test for nav transition when user loggs in

---
+++ spec/features/logged_in_user_sees_logout_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+describe "when a user loggs in" do
+  it "they 'login' turns into 'logout' on nav" do
+    user = User.create(first_name: "Billy",
+                last_name: "Goat",
+                password: "pass",
+                phone: "555-555-5555",
+                email: "billygoat@gmail.com",
+               )
+    visit login_path
+
+   fill_in "session[username]", with: "billygoat"
+   fill_in "session[password]", with: "pass"
+
+   expect(current_path).to eq(login_path)
+
+   within(".nav-wrapper") do
+     expect(page).to have_content("Logout")
+     expect(page).to_not have_content("Login")
+   end
+  end
+end
