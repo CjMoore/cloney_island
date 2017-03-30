a5835e872cb93ed08044726e7621d2a58cc3c4f7
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-03-29 18:42:24 -0600

    Update users.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :user do
-    first_name "MyString"
-    last_name "MyString"
-    password "MyString"
-    email "MyString"
-    phone "MyString"
-    username "MyUsername"
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    password { Faker::Team.state }
+    email { Faker::Internet.email }
+    phone { Faker::PhoneNumber.cell_phone }
+    username { Faker::Hacker.noun }
   end
 end
