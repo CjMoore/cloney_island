a4d6e3c73d8bc0ab9af9a18291b358387dce7277
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-03-31 12:30:27 -0600

    make test for registered user profile page

---
+++ spec/features/reg_user_profile_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe "Registered User" do
+  it "sees profile page" do
+    
+  end
+end
+
+# As a registered user
+# Who is logged in
+# When they click on profile
+# They are taken to ‘/username’
+# They see a personal image
+# They see their email
+# They see their username
+# They see an edit button
