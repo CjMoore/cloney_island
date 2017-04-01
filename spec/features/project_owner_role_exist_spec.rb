29e66884650cfede65fbc140d04c88e76ed64c2b
Spencer Carter <scmountain@gmail.com>
2017-03-31 18:56:52 -0600

    [wip] project creation

--- spec/features/project_owner_role_exist_spec.rb
+++ spec/features/project_owner_role_exist_spec.rb
@@ -10,14 +10,13 @@ describe "a registered user in the create project page" do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit "/projects/new"
-    save_and_open_page
 
-    fill_in projects["title"], with: "Goat farm"
-    fill_in projects["image url"], with: "http://www.babypygmygoats.com/wp-content/uploads/2015/05/baby-goat-jumping.jpg"
-    fill_in projects["description"], with: "best goat ever"
-    select projects["total time"], with: "1 Week"
-    fill_in projects["amount to raise"], with: "40000"
-    fill_in projects["Contributor Email"], with: "billy@goat.com"
+    fill_in "projects[title]", with: "Goat farm"
+    fill_in "projects[image_url]", with: "http://www.babypygmygoats.com/wp-content/uploads/2015/05/baby-goat-jumping.jpg"
+    fill_in "projects[description]", with: "best goat ever"
+    select "1 Week", from: "projects[total_time]"
+    fill_in "projects[amount_to_raise]", with: "40000"
+    fill_in "projects[Contributor Email]", with: "billy@goat.com"
 
     click_on "Create Project"
 
