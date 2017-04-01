6b17e3c5b7e3c9a79824fab5df1a17fa4d09fba8
Charlotte Moore <mooc123@gmail.com>
2017-04-01 14:54:57 -0600

    adds helper method for project owner permissions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def registered_user?
     current_user && current_user.registered_user?
   end
+
+  def project_owner?
+    current_user && current_user.project_owner?
+  end
 end
