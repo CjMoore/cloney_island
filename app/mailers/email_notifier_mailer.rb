6ab3abd7d8e277469ea8c3cb6bedcb5e190ccac5
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-04-02 16:05:38 -0600

    Update email_notifier_mailer.rb

--- app/mailers/email_notifier_mailer.rb
+++ app/mailers/email_notifier_mailer.rb
@@ -1,6 +1,6 @@
 class EmailNotifierMailer < ApplicationMailer
   def inform(current_user, contributor)
     @user = current_user
-    mail(to: contributor, subject: "#{user.name} would you like to you to help with a crowdfunding project.")
+    mail(to: contributor, subject: "#{user.name} would you like to help with a crowdfunding project?")
   end
 end
