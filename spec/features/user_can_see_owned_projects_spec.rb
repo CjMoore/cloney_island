e662d8b3a454d3e734cbb9d7fdcbb576f671cf9a
Edilene <edilene_cruz@yahoo.com.br>
2017-04-02 18:05:36 -0600

    implements user show page to have owned projects table

--- spec/features/user_can_see_owned_projects_spec.rb
+++ spec/features/user_can_see_owned_projects_spec.rb
@@ -4,9 +4,19 @@ describe "a project owner can see their owned projects in dashboard" do
     before(:each) do
       @user = create(:user)
       registered_role = create(:role)
+      project_1 = create(:project, name: "Guitars")
+      project_2 = create(:project, name: "Tiki Bars")
+      project_3 = create(:project, name: "Conch Fritters")
+      project_4 = create(:project, name: "Turing Bootcamp")
+      @user.owned_projects << project_1
+      @user.owned_projects << project_2
+      @user.owned_projects << project_3
       project_owner = Role.create(name: "project_owner")
       @user.roles << registered_role
       @user.roles << project_owner
+      @user.roles << Role.create!(name: "project_funder")
+      UserFundedProject.create(project: project_4, user: @user, amount: 4000, credit_card_number: Faker::Business.credit_card_number)
+
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     end
@@ -23,16 +33,18 @@ describe "a project owner can see their owned projects in dashboard" do
 
     visit "/#{@user.slug}"
 
-    # save_and_open_page
+
+    within(".owned-projects") do
+    expect(page).to have_content("Guitars")
+    expect(page).to have_content("Tiki Bar")
+    expect(page).to have_content("Conch Fritters")
+    expect(page).to have_content("Project Name")
+    expect(page).to have_content("Remaining Time")
+    expect(page).to have_content("Total Amount Funded")
+    expect(page).to have_content("Total Amount Needed")
+    expect(page).to have_link("Edit")
+    expect(page).to have_link("Disable")
+    save_and_open_page
+    end
   end
 end
-
-
-# They see a table with all of their owned projects
-# The table includes project name
-# Each project name is a link to the project show
-# Total amount
-# Total Funded
-# Remaining Time
-# There is a button labeled “Edit”
-# There is a button labeled “Disable"
