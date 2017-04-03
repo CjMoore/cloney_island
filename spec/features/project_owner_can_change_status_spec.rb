4dbd8f3a54c9f14503ac8b88d9f0ed577eee6729
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-04-03 14:13:54 -0600

    update permissions so project owner can create

--- spec/features/project_owner_can_change_status_spec.rb
+++ spec/features/project_owner_can_change_status_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 describe "Project Owner" do
   it "can change status of project from " do
-
+    #create user
+    #create project0 associated with user, with user as owner
+    #set project0 status to active
+    #create project1 associated with user, with user as owner
+    #set project1 status to disabled
+    #stub login
+    #visit /username
+    #expect page to have 
   end
 end
