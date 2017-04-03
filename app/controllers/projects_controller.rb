edfc7e0e1d44aeb90925c00fb7aa4d891798497f
Edilene <edilene_cruz@yahoo.com.br>
2017-04-03 17:05:13 -0600

    fixed create project with collaborator

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -13,26 +13,55 @@ class ProjectsController < ApplicationController
   end
 
   def create
-    @user = current_user
+    user = current_user
+    data = params
+    if params[:contributor_email].empty?
+      create_without_contributor(user, data)
+    else
+      is_valid_contributor(user, data)
+    end
+  end
+
+  def create_without_contributor(user, data)
+    @user = user
     @project = Project.new(project_params)
     days = params[:time][:month].to_i
-    @project.update_attribute(:time, (Time.now.to_date + days) )
+    @project.time = (Time.now.to_date + days)
     if @project.save
       @role = Role.find_or_create_by(name: "project_owner")
       @user.roles << @role
-      @project.owners << @user
-      @contributor = User.find_by(email: params[:contributor_email])
-        if !@contributor.nil?
-          @contributor.roles << @role
-          @project.owners << @contributor
-          flash.now[:notice] = "You just added a new project owner."
-        else
-          flash.now[:warning] = "This email doesn't exist in our database and we cannot add them as a project owner."
-        end
-      flash[:notice] = "Your project has been created!"
+      flash[:notice] = "You just created a project without a contributor!"
       redirect_to username_path(@user.username)
     else
       flash[:warning] = "Please try again."
+      render :new
+    end
+  end
+
+  def is_valid_contributor(user, data)
+    contributor = User.find_by(email: params[:contributor_email])
+     if !contributor.nil?
+      create_with_valid_contributor(user, data, contributor)
+    else
+      flash[:warning] = "The email doesn't exit in our database."
+      render :new
+    end
+  end
+
+  def create_with_valid_contributor(user, data, contributor)
+    @user = user
+    @project = Project.new(project_params)
+    days = params[:time][:month].to_i
+    @project.time = (Time.now.to_date + days)
+    @contributor = contributor
+    if @project.save
+      @role = Role.find_or_create_by(name: "project_owner")
+      @user.roles << @role
+      @contributor.roles << @role
+      flash[:notice] = "A project was created with #{@contributor.first_name} as a joint owner."
+      redirect_to username_path(@user.username)
+    else
+      flash[:warning] = "Please add a valid email for the contributor."
       render :new
     end
   end
