561f18434b1f931768a6af5caa941e61df3e0bf6
Charlotte Moore <mooc123@gmail.com>
2017-04-04 13:42:35 -0600

    admin can revoke admin status

--- spec/features/admin/admin_can_add_admin_spec.rb
+++ spec/features/admin/admin_can_add_admin_spec.rb
@@ -16,6 +16,7 @@ describe "when addmin is on user index" do
 
     visit users_path
 
+    expect(user1.roles.count).to eq(1)
     table_rows = page.all('.users-table tr')
 
     within(table_rows[2]) do
@@ -31,5 +32,38 @@ describe "when addmin is on user index" do
     within('.users-table') do
       expect(page).to_not have_button("Make Admin")
     end
+
+    expect(user1.roles.count).to eq(2)
+  end
+
+  it "an admin can revoke admin privileges" do
+    reg_user = create(:role, id: 1)
+    proj_owner = create(:role, name: "project_owner", id: 2)
+    proj_funder = create(:role, name: "project_funder", id: 3)
+    admin_user = create(:role, name: "admin_user", id: 4)
+    admin = create(:user)
+    admin.roles << admin_user
+
+    user1 = create(:user, username: "takethepower")
+    user1.roles << reg_user
+    user1.roles << admin_user
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit users_path
+
+    table_rows = page.all('.users-table tr')
+
+    within(table_rows[2]) do
+      click_on('Revoke Admin')
+    end
+
+    within('.user-info') do
+      within('.num-admins') do
+        expect(page).to have_content(1)
+      end
+    end
+
+    expect(user1.roles.count).to eq(1)
   end
 end
