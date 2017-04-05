791a1e7000c10b3fe61b2e967cc6ef95884090f0
Spencer Carter <scmountain@gmail.com>
2017-04-04 22:03:20 -0600

    adds method model testing for UserRole

--- spec/models/user_role_spec.rb
+++ spec/models/user_role_spec.rb
@@ -10,4 +10,23 @@ RSpec.describe UserRole, type: :model do
     it { should belong_to(:user) }
     it { should belong_to(:role) }
   end
+
+  it "methods" do
+    user = create(:user)
+    user2 = create(:user)
+    user3 = create(:user)
+    user4 = create(:user)
+    funder = Role.create(name: "project_funder", id: 3)
+    admin = Role.create(name: "admin_user", id: 4)
+    owner = Role.create(name: "project_owner", id: 2)
+    reg = Role.create(name: "registered_user", id: 1)
+    user.roles << funder
+    user2.roles << admin
+    user3.roles << owner
+    user4.roles << reg
+    expect(UserRole.num_of_admins).to eq(1)
+    expect(UserRole.num_of_project_owners).to eq(1)
+    expect(UserRole.num_of_project_funders).to eq(1)
+    expect(UserRole.num_of_registered_users).to eq(1)
+  end
 end
