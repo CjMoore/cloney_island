0297fa3aac47a26f24704e40e411b12617b149c3
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-04-05 16:50:26 -0600

    write passing feature test for status funded

--- spec/features/projects/funded_status_show_spec.rb
+++ spec/features/projects/funded_status_show_spec.rb
@@ -1,27 +1,21 @@
 require 'rails_helper'
 
 describe "Funded Project" do
-  it "shows projects over total funding" do
+  it "shows funded on project show" do
     user = create(:user)
     role = create(:role, name: "project_funder" )
     user.roles << role
     project  = create(:project)
-    project2 = create(:project)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     UserFundedProject.create(project: project, user: user, amount: 10000000, credit_card_number: Faker::Business.credit_card_number)
-    UserFundedProject.create(project: project2, user: user, amount: 2, credit_card_number: Faker::Business.credit_card_number)
 
-    visit "/projects"
+    visit "/projects/#{project.slug}"
 
-    funded = page.all(".card-action")
+    funded = page.all("#welcome")
 
     within(funded[0]) do
       expect(page).to have_content("Funded!")
-    end
-
-    within(funded[1]) do
-      expect(page).to_not have_content("Funded!")
     end
   end
 end
