2c1bd21dad277a95ddfb0a917c6f7f9167df5893
Charlotte Moore <mooc123@gmail.com>
2017-04-05 18:11:34 -0600

    validates user funded project creation

--- app/controllers/user_funded_projects_controller.rb
+++ app/controllers/user_funded_projects_controller.rb
@@ -8,11 +8,16 @@ class UserFundedProjectsController < ApplicationController
   def create
     @user = current_user
     @project = Project.find(params[:project_id])
-    fund = @user.user_funded_projects.create(project_params)
-    @role = Role.find_or_create_by(name: "project_funder")
-    @user.roles << @role
-    flash.now[:notice] = "You just funded #{@project.name} for #{fund.amount}!"
-    redirect_to project_path(@project.slug)
+    @fund = @user.user_funded_projects.new(project_params)
+    if @fund.save
+      @role = Role.find_or_create_by(name: "project_funder")
+      @user.roles << @role
+      flash.now[:notice] = "You just funded #{@project.name} for #{@fund.amount}!"
+      redirect_to project_path(@project.slug)
+    else
+      flash[:error] = @fund.errors.full_messages.to_sentence
+      render :new
+    end
   end
 
   private
